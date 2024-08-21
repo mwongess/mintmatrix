0ff13e9461b4b3c9568919f5f6a6b663bd799f37
mwongess <amosmwongelah@gmail.com>
2024-08-21 17:05:48 +0300

    add charts

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,7 +1,18 @@
+import { MonthRevenue } from "./charts/MonthRevenue"
+
 const Statistics = () => {
-  return (
-    <div>Statistics</div>
-  )
+    return (
+        <div>
+            <div className="grid grid-cols-4 gap-4">
+                <MonthRevenue />
+                <MonthRevenue />
+                <div className="col-span-2 h-60">
+
+                <MonthRevenue />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Statistics
